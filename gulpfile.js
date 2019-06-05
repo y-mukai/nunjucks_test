9f5f27d63c93f116b91fdacfb5ee5228e1e5cb8a
mukai <yuka.mukai@accenture.com>
2019-06-05 17:51:12 +0900

    [wip] 応用

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,37 @@
 const gulp  = require('gulp'),
-      $     = require('gulp-load-plugins')();
+      $     = require('gulp-load-plugins')(),
+      sync  = require('browser-sync');
 
 gulp.task('html', () => {
   return gulp
     .src([
-      'src/nunjucks/*.html',
-      '!src/nunjucks/_*.html'
+      'src/nunjucks/*.njk',
+      '!src/nunjucks/_*.njk'
     ])
+    .pipe($.data( ()=> {
+      return require('./src/json/data.json')
+    }))
     .pipe($.nunjucksRender({
       path: ['src/nunjucks/']
     }))
     .pipe(gulp.dest('dist'));
 })
+
+gulp.task('server', (done) => {
+  sync.init({
+    server: {
+      baseDir: './dist/'
+    }
+  });
+  done();
+})
+
+gulp.task('watch', (done) => {
+  const reload = () => {
+    sync.reload();
+    done();
+  }
+  gulp.watch('src/nunjucks/*.njk').on('change', gulp.series('html', reload));
+})
+
+gulp.task('default', gulp.series('html', 'server', 'watch'))
